d980912da2cec53cf5ed3d00f85496a5b94fc16b
umutbayraktar <umt9735@gmail.com>
2019-06-23 01:14:43 +0300

    new test cases created

--- game.py
+++ game.py
@@ -1,6 +1,26 @@
 
-def parseInput(input):
+def parseInput(input_string):
 
-    if input == '' or input is None:
+    if input_string == '' or input_string is None:
         return 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz'
+
+    if ',' in input_string:
+        parsed_input=input_string.split(',')
+        if len(parsed_input) != 2:
+            return 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz'
+        else: #len == 2 x,y
+            try:
+                x = int(parsed_input[0])
+                y = int(parsed_input[1])
+                return {'x':x,'y': y}
+            except:
+                return 'Lütfen nümerik veri giriniz'
+
+
+
+
+    else:
+        return 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz'
+
+
 
--- tests.py
+++ tests.py
@@ -4,15 +4,24 @@ from game import parseInput
 class InputTest(unittest.TestCase):
 
 
-    def test_trueInput(self):
-        self.assertEqual(parseInput('5,3'),{'x':5,'y':3})
+    def test_true_input(self):
+        self.assertEqual(parseInput('5,3'), {'x': 5,'y': 3})
 
     def test_empty(self):
-        self.assertEqual(parseInput(''),'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+        self.assertEqual(parseInput(''), 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
 
     def test_null(self):
         self.assertEqual(parseInput(''), 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
 
     def test_string(self):
-        self.assertEqual(parseInput('dsdsad'),'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+        self.assertEqual(parseInput('dsdsad'), 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+
+    def test_more_comma(self):
+        self.assertEqual(parseInput('sas,sas,asa'), 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+
+    def test_numerik_and_string(self):
+        self.assertEqual(parseInput('3,asasa'), 'Lütfen nümerik veri giriniz')
+
+    def test_x_negative(self):
+        self.assertEqual(parseInput('-3,5'),'İndis değeri negatif olamaz')
 
