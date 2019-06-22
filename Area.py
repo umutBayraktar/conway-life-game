51d89671881023e326559ca71872f8266c781c6f
umutbayraktar <umt9735@gmail.com>
2019-06-23 02:19:23 +0300

    Area class created

---
+++ Area.py
@@ -0,0 +1,21 @@
+from .Cell import Cell
+
+
+class Area:
+
+    array = range(0, 8)
+    cells = [[], [], [], [], [], [], [], []]
+
+    def __init__(self):
+        for i in self.array:
+            for j in self.array:
+                self.cells[i].append(Cell())
+
+    def set_cell_live(self,x,y):
+        pass
+
+    def print_area(self):
+        pass
+
+    def examine_environment(self):
+        pass
