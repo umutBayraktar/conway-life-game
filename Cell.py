b8ef30296d5662d2d436584f0a04ed2633b8bb94
umutbayraktar <umt9735@gmail.com>
2019-06-23 01:56:52 +0300

    cell class created

---
+++ Cell.py
@@ -0,0 +1,25 @@
+
+class Cell():
+
+    x = 0
+    y = 0
+    prev_status=False
+    current_status=False
+
+    def update_status(self):
+        pass
+
+    def print_status(self):
+        pass
+
+    def is_live(self):
+        pass
+
+    def is_dead(self):
+        pass
+
+    def dead(self):
+        pass
+
+    def revive(self):
+        pass
