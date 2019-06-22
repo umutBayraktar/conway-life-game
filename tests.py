6c120152a8d171a3f934cbb5c5df55cc75ebf1fc
umutbayraktar <umt9735@gmail.com>
2019-06-23 00:51:34 +0300

    true input,null and string input cases created

--- tests.py
+++ tests.py
@@ -3,5 +3,16 @@ from game import parseInput
 
 class InputTest(unittest.TestCase):
 
+
+    def test_trueInput(self):
+        self.assertEqual(parseInput('5,3'),{'x':5,'y':3})
+
     def test_empty(self):
         self.assertEqual(parseInput(''),'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+
+    def test_null(self):
+        self.assertEqual(parseInput(''), 'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+
+    def test_string(self):
+        self.assertEqual(parseInput('dsdsad'),'Yanlış girdi,lütfen koordinat bilgisini x,y şeklinde giriniz')
+
