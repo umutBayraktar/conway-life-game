11307ef87d6fee7087d6248c73cb3b94d6a2272a
umutbayraktar <umt9735@gmail.com>
2019-06-24 01:44:24 +0300

    upgrade status method passed tests succesfull

--- Cell.py
+++ Cell.py
@@ -19,7 +19,7 @@ class Cell():
     def set_status(self, neighbour_count):
         if neighbour_count < 2 or neighbour_count > 3:
             self.dead()
-        elif neighbour_count is 3 and self.prev_status is False:
+        elif neighbour_count is 3 and self.is_dead():
             self.revive()
         return self.current_status
 
@@ -37,3 +37,7 @@ class Cell():
     def revive(self):
         self.current_status = True
         return self.current_status
+
+    def upgrade_status(self):
+        self.prev_status=self.current_status
+        return self.prev_status
